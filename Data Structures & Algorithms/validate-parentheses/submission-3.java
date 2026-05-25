0c680a5dab2c2f62017dcb98805e4076eddbfecc
Anish Sivakumar <73561392+anish-siv@users.noreply.github.com>
2026-05-25 16:58:22 -0500

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean isValid(String s) {
+        ArrayDeque<Character> charStack = new ArrayDeque<>();
+
+        char[] charArray = s.toCharArray();
+
+        for(char i : charArray) {
+            if(i == '(') {
+                charStack.push(i);
+            }
+            if(i == '[') {
+                charStack.push(i);
+            }
+            if(i == '{') {
+                charStack.push(i);
+            }
+            if(i == ')') {
+                if(charStack.isEmpty()) {
+                    return false;
+                }
+                if(charStack.peek() == '(') {
+                    charStack.pop();
+                } else {
+                    return false;
+                }
+            }
+            if(i == ']') {
+                if(charStack.isEmpty()) {
+                    return false;
+                }
+                if(charStack.peek() == '[') {
+                    charStack.pop();
+                } else {
+                    return false;
+                }
+            }
+            if(i == '}') {
+                if(charStack.isEmpty()) {
+                    return false;
+                }
+                if(charStack.peek() == '{') {
+                    charStack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return charStack.isEmpty();
+    }
+}
